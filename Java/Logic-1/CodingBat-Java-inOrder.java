74a4aad355fb08a1a3e1de644e51b983add84665
Ozlem <cse.ozlemkorpe@gmail.com>
2020-07-29 05:41:52 +0300

    Logic-1

---
+++ Java/Logic-1/CodingBat-Java-inOrder.java
@@ -0,0 +1,20 @@
+/*
+Project: Solution of CodingBat Logic-1 inOrder by using Java.
+Date: 29.07.2020
+Author: OzlemKorpe
+*/
+/*
+Given three ints, a b c, return true if b is greater than a, and c is greater than b. However, with the exception that if "bOk" is true, b does not need to be greater than a.
+
+inOrder(1, 2, 4, false) → true
+inOrder(1, 2, 1, false) → false
+inOrder(1, 1, 2, true) → true
+*/
+
+public boolean inOrder(int a, int b, int c, boolean bOk) {
+  if(!bOk && b>a && c>b){
+    return true;
+  }else if(bOk && c>b){
+    return true;
+  } return false;
+}
